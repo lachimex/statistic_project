f7b50930d5318fe8609ddf4b1e41c25b46718542
Michał Dydek <dydmichal@gmail.com>
2023-12-08 23:41:12 +0100

    first model for linear regression

--- linear_regression/model.py
+++ linear_regression/model.py
@@ -15,19 +15,23 @@ b = 1
 learning_rate = 0.01
 
 
-epochs = 300
+epochs = 1000
 
 for _ in range(epochs):
     dl_da = 0
     dl_db = 0
     for i in range(x_values.size):
-        dl_da += 2 * (a * x_values[i] + b - y_values[i]) * x_values[i]
-        dl_db += 2 * (a * x_values[i] + b - y_values[i])
-    dl_da /= x_values.size
-    dl_db /= x_values.size
+        dl_da += 2/x_values.size * (a * x_values[i] + b - y_values[i]) * x_values[i]
+        dl_db += 2/x_values.size * (a * x_values[i] + b - y_values[i])
     #gradient descent
     a -= dl_da * learning_rate
     b -= dl_db * learning_rate
+
+print(a, b)
+error = 0
+for i in range(x_values.size):
+    error += (a * x_values[i] + b - y_values[i]) ** 2
+print(error)
 
 plt.scatter(x_values, y_values)
 plt.plot(list(range(1, 12)), [a * x + b for x in range(1, 12)], color="red")
